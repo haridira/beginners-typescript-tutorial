62978a4af1af213720bc32459cc76fd3ca0bbfda
Haritha Diraneyya <haritha.diraneyya@gmail.com>
2023-12-09 03:50:01 -0500

    update

--- src/01-number.problem.ts
+++ src/01-number.problem.ts
@@ -1,6 +1,8 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (a, b) => {
+
+// added the number type
+export const addTwoNumbers = (a: number, b: number) => {
   return a + b;
 };
 
